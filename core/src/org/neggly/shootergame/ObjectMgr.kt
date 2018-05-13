f28d815106486c77347b536eff51c073dbc86ad9
negset <negset1858@gmail.com>
2018-05-13 17:29:08 +0900

    refactroing

--- core/src/org/neggly/shootergame/ObjectMgr.kt
+++ core/src/org/neggly/shootergame/ObjectMgr.kt
@@ -1,6 +1,5 @@
 package org.neggly.shootergame
 
-import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.math.Circle
@@ -9,6 +8,11 @@ import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.Group
 
+/**
+ * すべてのゲームオブジェクトのインスタンスを保有・管理するオブジェクト.
+ *
+ * @author negset
+ */
 object ObjectMgr : Group()
 {
     private lateinit var assets: AssetManager
@@ -20,11 +24,21 @@ object ObjectMgr : Group()
     private lateinit var shots: Array<Shot>
     private lateinit var items: Array<Item>
 
+    /** スコア */
     var score = 0
+    /** 残機 */
     var life = 3
+    /** ゲームオーバーか否か */
     var isGameOver = false
+    /** ボス戦中か否か */
     var bossTime = false
 
+    /**
+     * 初期化処理を行う.
+     * アセットの読み込み命令も行う.
+     *
+     * @param assetManager アセットマネージャ
+     */
     fun init(assetManager: AssetManager)
     {
         assets = assetManager
@@ -41,6 +55,9 @@ object ObjectMgr : Group()
         bossTime = false
     }
 
+    /**
+     *
+     */
     fun setAssets()
     {
         player = Player(assets.get("player.png"))
@@ -74,6 +91,9 @@ object ObjectMgr : Group()
         collisionDetectionItemToPlayer()
     }
 
+    /**
+     * 自機弾とボスの衝突判定を行う.
+     */
     private fun collisionDetectBulletAndBoss()
     {
         if (!boss.hasParent()) return
@@ -91,6 +111,9 @@ object ObjectMgr : Group()
         }
     }
 
+    /**
+     * 自機弾と敵機の衝突判定を行う.
+     */
     private fun collisionDetectionBulletAndEnemy()
     {
         for (enemy in enemies.filter { it.hasParent() })
@@ -108,6 +131,9 @@ object ObjectMgr : Group()
         }
     }
 
+    /**
+     * 自機とボスの衝突判定を行う.
+     */
     private fun collisionDetectionPlayerAndBoss()
     {
         if (player.isInvincible) return
@@ -121,6 +147,9 @@ object ObjectMgr : Group()
         }
     }
 
+    /**
+     * 敵機弾と自機の衝突判定を行う.
+     */
     private fun collisionDetectionShotAndPlayer()
     {
         if (player.isInvincible) return
@@ -136,6 +165,9 @@ object ObjectMgr : Group()
         }
     }
 
+    /**
+     * 自機と敵機の衝突判定を行う.
+     */
     private fun collisionDetectionPlayerAndEnemy()
     {
         if (player.isInvincible) return
@@ -151,6 +183,9 @@ object ObjectMgr : Group()
         }
     }
 
+    /**
+     * 自機とアイテムの衝突判定を行う.
+     */
     private fun collisionDetectionItemToPlayer()
     {
         val circle1 = Circle(player.x, player.y, 20f)
@@ -169,48 +204,48 @@ object ObjectMgr : Group()
         }
     }
 
+    /**
+     * 新規に自機弾を生成する.
+     * 実際は配列から無効なものを探して有効化する.
+     */
     fun newBullet(x: Float, y: Float)
     {
-        for (bullet in bullets.filter { !it.hasParent() })
-        {
-            bullet.activate(x, y)
-            addActor(bullet)
-            return
-        }
-        Gdx.app.error("ObjectMgr", "no free bullet")
+        val bullet = bullets.filterNot { it.hasParent() }.first()
+        bullet.activate(x, y)
+        addActor(bullet)
     }
 
+    /**
+     * 新規に敵機弾を生成する.
+     * 実際は配列から無効なものを探して有効化する.
+     */
     fun newShot(x: Float, y: Float, deg: Float)
     {
-        for (shot in shots.filter { !it.hasParent() })
-        {
-            shot.activate(x, y, deg)
-            addActor(shot)
-            return
-        }
-        Gdx.app.error("ObjectMgr", "no free shot")
+        val shot = shots.filterNot { it.hasParent() }.first()
+        shot.activate(x, y, deg)
+        addActor(shot)
     }
 
+    /**
+     * 新規に敵機を生成する.
+     * 実際は配列から無効なものを探して有効化する.
+     */
     fun newEnemy(x: Float, y: Float)
     {
-        for (enemy in enemies.filter { !it.hasParent() })
-        {
-            enemy.activate(x, y)
-            addActor(enemy)
-            return
-        }
-        Gdx.app.error("ObjectMgr", "no free enemy")
+        val enemy = enemies.filterNot { it.hasParent() }.first()
+        enemy.activate(x, y)
+        addActor(enemy)
     }
 
+    /**
+     * 新規にアイテムを生成する.
+     * 実際は配列から無効なものを探して有効化する.
+     */
     fun newItem(x: Float, y: Float)
     {
-        for (item in items.filter { !it.hasParent() })
-        {
-            item.activate(x, y)
-            addActor(item)
-            return
-        }
-        Gdx.app.error("ObjectMgr", "no free item")
+        val item = items.filterNot { it.hasParent() }.first()
+        item.activate(x, y)
+        addActor(item)
     }
 
     fun getAngle(obj1: GameObject, obj2: GameObject): Float
@@ -241,6 +276,10 @@ object ObjectMgr : Group()
         return enemies.filter { it.hasParent() }.isNotEmpty()
     }
 
+    /**
+     * リソースの解放処理.
+     * 子Actorの削除やアセットの解放を行う.
+     */
     fun dispose()
     {
         clearChildren()
