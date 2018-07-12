fb918e03db8dd9f05df906556bd651f94fa98538
negset <negset1858@gmail.com>
2018-07-13 00:47:51 +0900

    To use scene2d stage & refactoring

--- core/src/org/neggly/shootergame/TitleScreen.kt
+++ core/src/org/neggly/shootergame/TitleScreen.kt
@@ -1,51 +1,41 @@
 package org.neggly.shootergame
 
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.Screen
-import com.badlogic.gdx.assets.AssetManager
-import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator
-import com.badlogic.gdx.math.Vector3
-import com.badlogic.gdx.utils.viewport.FitViewport
-import com.badlogic.gdx.utils.viewport.Viewport
-import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGeneratorLoader
-import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver
+import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.utils.Align
+import com.badlogic.gdx.utils.viewport.FitViewport
 
 
 class TitleScreen(game: ShooterGame) : ScreenAdapter(game)
 {
-    private var uiCamera: OrthographicCamera? = null
-    private var viewport: Viewport? = null
-    private var batch: SpriteBatch? = null
+    private val stage = Stage(FitViewport(WIDTH, HEIGHT))
+    private val batch = SpriteBatch()
 
-    private var font: BitmapFont? = null
+    private lateinit var font: BitmapFont
 
-    private var assetHasSet = false
+    private var isAssetsUnset = true
 
     override fun show()
     {
-        uiCamera = OrthographicCamera()
-        uiCamera?.setToOrtho(false, WIDTH, HEIGHT)
-        viewport = FitViewport(WIDTH, HEIGHT, uiCamera)
-        batch = SpriteBatch()
+        Gdx.input.inputProcessor = stage
+        batch.projectionMatrix = stage.camera.combined
     }
 
     override fun render(delta: Float)
     {
-        if (!assetHasSet) setAsset()
+        if (isAssetsUnset)
+            setAssets()
+
         update(delta)
         draw()
     }
 
     private fun update(delta: Float)
     {
+        stage.act(delta)
+
         if (Gdx.input.justTouched())
         {
             game.nextScreen = PlayScreen(game)
@@ -54,22 +44,16 @@ class TitleScreen(game: ShooterGame) : ScreenAdapter(game)
 
     private fun draw()
     {
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        uiCamera?.update()
-        batch?.projectionMatrix = uiCamera?.combined
-
-        batch?.begin()
-        font?.draw(batch, "シューティング", 0f, 1900f, WIDTH, Align.center, true)
-        font?.draw(batch, "ゲーム", 0f, 1750f, WIDTH, Align.center, true)
-        font?.draw(batch, "タッチで開始", 0f, 1000f, WIDTH, Align.center, true)
-        font?.draw(batch, "© 2018 negset", 0f, 300f, WIDTH, Align.center, true)
-        batch?.end()
+        batch.begin()
+        font.draw(batch, "シューティングゲーム", 0f, 1900f, WIDTH, Align.center, true)
+        font.draw(batch, "タッチで開始", 0f, 1000f, WIDTH, Align.center, true)
+        font.draw(batch, "(C) 2018 negset", 0f, 300f, WIDTH, Align.center, true)
+        batch.end()
     }
 
     override fun resize(width: Int, height: Int)
     {
-        viewport?.update(width, height)
+        stage.viewport.update(width, height)
     }
 
     override fun hide()
@@ -79,12 +63,13 @@ class TitleScreen(game: ShooterGame) : ScreenAdapter(game)
 
     override fun dispose()
     {
-        batch?.dispose()
+        stage.dispose()
+        batch.dispose()
     }
 
-    private fun setAsset()
+    private fun setAssets()
     {
-        font = game.assets.get("default.otf")
-        assetHasSet = true
+        font = game.assets.get("font.ttf")
+        isAssetsUnset = false
     }
 }
