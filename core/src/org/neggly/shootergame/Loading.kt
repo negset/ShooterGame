95f4cb3c712466063a4b83a331d30183899b844d
negset <negset1858@gmail.com>
2018-07-13 00:44:40 +0900

    To use scene2d stage

--- core/src/org/neggly/shootergame/Loading.kt
+++ core/src/org/neggly/shootergame/Loading.kt
@@ -1,45 +1,31 @@
 package org.neggly.shootergame
 
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.utils.viewport.FitViewport
-import com.badlogic.gdx.utils.viewport.Viewport
 
 class Loading
 {
-    private var uiCamera: OrthographicCamera? = null
-    private var viewport: Viewport? = null
-    private var batch: SpriteBatch? = null
-
-    private val texture = Texture("loading.png")
+    private var stage: Stage = Stage(FitViewport(WIDTH, HEIGHT))
 
     init
     {
-        uiCamera = OrthographicCamera()
-        uiCamera?.setToOrtho(false, WIDTH, HEIGHT)
-        viewport = FitViewport(WIDTH, HEIGHT, uiCamera)
-        batch = SpriteBatch()
-
-        texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear)
+        val tex = Texture("loading.png")
+        tex.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear)
+        val image = Image(tex)
+        image.setPosition(100f, 100f)
+        stage.addActor(image)
     }
 
     fun render()
     {
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        uiCamera?.update()
-        batch?.projectionMatrix = uiCamera?.combined
-
-        batch?.begin()
-        batch?.draw(texture, 0f, 0f)
-        batch?.end()
+        stage.act()
+        stage.draw()
     }
 
     fun dispose()
     {
-        texture.dispose()
+        stage.dispose()
     }
 }
