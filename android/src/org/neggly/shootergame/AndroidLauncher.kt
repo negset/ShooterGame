79f40f3468b91f0f8584355f358ba04f4942cf82
negset <negset1858@gmail.com>
2018-07-17 00:10:46 +0900

    Refactoring

--- android/src/org/neggly/shootergame/AndroidLauncher.kt
+++ android/src/org/neggly/shootergame/AndroidLauncher.kt
@@ -1,7 +1,6 @@
 package org.neggly.shootergame
 
 import android.os.Bundle
-
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 
