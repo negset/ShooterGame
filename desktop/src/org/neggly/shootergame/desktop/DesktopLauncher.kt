afaef7d9ade44546285802d5ac62cf476dd4f201
negset <negset1858@gmail.com>
2018-07-17 00:10:30 +0900

    Convert Java to Kotlin

---
+++ desktop/src/org/neggly/shootergame/desktop/DesktopLauncher.kt
@@ -0,0 +1,18 @@
+package org.neggly.shootergame.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import org.neggly.shootergame.ShooterGame
+
+object DesktopLauncher
+{
+    @JvmStatic
+    fun main(arg: Array<String>)
+    {
+        val config = LwjglApplicationConfiguration()
+        config.width = 540
+        config.height = 960
+        config.resizable = false
+        LwjglApplication(ShooterGame(), config)
+    }
+}
