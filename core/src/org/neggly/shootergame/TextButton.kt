5919bdf2bf9afe79d68f35191cc4cbdd6eedbc8e
negset <negset1858@gmail.com>
2018-07-18 06:06:19 +0900

    Create TextButton

---
+++ core/src/org/neggly/shootergame/TextButton.kt
@@ -0,0 +1,51 @@
+package org.neggly.shootergame
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.NinePatch
+import com.badlogic.gdx.math.Rectangle
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.utils.Align
+
+class TextButton : Actor()
+{
+    var bg: NinePatch? = null
+    var font: BitmapFont? = null
+    var text: String = ""
+
+    private val bounds = Rectangle()
+
+    private var touchPoint = Vector2()
+
+    override fun draw(batch: Batch, parentAlpha: Float)
+    {
+        batch.setColor(color.r, color.g, color.b, color.a * parentAlpha)
+        bg?.draw(batch, x - width / 2, y - height / 2, width, height)
+
+        if (font != null)
+            font!!.draw(batch, text, 0f, y + font!!.xHeight / 2, WIDTH, Align.center, true)
+    }
+
+    fun isClicked(): Boolean
+    {
+        if (Gdx.input.justTouched())
+        {
+            touchPoint.set(Gdx.input.x.toFloat(), Gdx.input.y.toFloat())
+            stage.screenToStageCoordinates(touchPoint)
+            return bounds.contains(touchPoint)
+        }
+        return false
+    }
+
+    override fun positionChanged()
+    {
+        bounds.setPosition(x - width / 2, y - height / 2)
+    }
+
+    override fun sizeChanged()
+    {
+        bounds.set(x - width / 2, y - height / 2, width, height)
+    }
+}
--- core/src/org/neggly/shootergame/TitleScreen.kt
+++ core/src/org/neggly/shootergame/TitleScreen.kt
@@ -1,12 +1,13 @@
 package org.neggly.shootergame
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.NinePatch
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.utils.Align
 import com.badlogic.gdx.utils.viewport.FitViewport
-
 
 class TitleScreen(game: ShooterGame) : ScreenAdapter(game)
 {
@@ -17,6 +18,9 @@ class TitleScreen(game: ShooterGame) : ScreenAdapter(game)
 
     private val bg = Bg()
 
+    private val playBtn = TextButton()
+    private val creditsBtn = TextButton()
+
     private var isAssetsUnset = true
 
     override fun show()
@@ -26,6 +30,10 @@ class TitleScreen(game: ShooterGame) : ScreenAdapter(game)
 
         bg.loadAssets(game.assets)
         stage.addActor(bg)
+
+        game.assets.load("button_bg.png", Texture::class.java)
+        stage.addActor(playBtn)
+        stage.addActor(creditsBtn)
     }
 
     override fun render(delta: Float)
@@ -41,10 +49,10 @@ class TitleScreen(game: ShooterGame) : ScreenAdapter(game)
     {
         stage.act(delta)
 
-        if (Gdx.input.justTouched())
-        {
+        if (playBtn.isClicked())
             game.nextScreen = PlayScreen(game)
-        }
+        else if (creditsBtn.isClicked())
+            game.nextScreen = CreditsScreen(game)
     }
 
     private fun draw()
@@ -53,7 +61,6 @@ class TitleScreen(game: ShooterGame) : ScreenAdapter(game)
 
         batch.begin()
         font.draw(batch, "シューティングゲーム", 0f, 1900f, WIDTH, Align.center, true)
-        font.draw(batch, "タッチで開始", 0f, 1000f, WIDTH, Align.center, true)
         font.draw(batch, "(C) 2018 negset", 0f, 300f, WIDTH, Align.center, true)
         batch.end()
     }
@@ -73,11 +80,33 @@ class TitleScreen(game: ShooterGame) : ScreenAdapter(game)
         stage.dispose()
         batch.dispose()
         bg.dispose()
+        game.assets.unload("button_bg.png")
     }
 
     private fun setAssets()
     {
         font = game.assets.get("font.ttf")
+
+        val btnBgTex: Texture = game.assets.get("button_bg.png")
+        btnBgTex.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear)
+        val btnBg = NinePatch(btnBgTex, 64, 64, 64, 64)
+
+        playBtn.apply {
+            bg = btnBg
+            font = game.assets.get("font.ttf")
+            text = "はじめる"
+            setPosition(WIDTH / 2, 1050f)
+            setSize(600f, 180f)
+        }
+
+        creditsBtn.apply {
+            bg = btnBg
+            font = game.assets.get("font.ttf")
+            text = "クレジット"
+            setPosition(WIDTH / 2, 750f)
+            setSize(600f, 180f)
+        }
+
         isAssetsUnset = false
     }
 }
