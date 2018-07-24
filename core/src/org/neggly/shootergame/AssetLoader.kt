a0fca75fe7194a7a76105319a1a1bfbe205a2a81
negset <negset1858@gmail.com>
2018-07-24 13:18:29 +0900

    Refactoring

--- core/src/org/neggly/shootergame/AssetLoader.kt
+++ core/src/org/neggly/shootergame/AssetLoader.kt
@@ -4,7 +4,7 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.utils.XmlReader
 
-class AssetLoader(private val xmlPath: String)
+class AssetLoader(xmlPath: String)
 {
     val manager = AssetManager()
 
