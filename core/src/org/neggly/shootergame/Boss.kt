16e79a0c25740afcfd7025b94de1b49bac4a7704
negset <negset1858@gmail.com>
2018-08-01 05:33:28 +0900

    Refactoring

--- core/src/org/neggly/shootergame/Boss.kt
+++ core/src/org/neggly/shootergame/Boss.kt
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.math.Circle
 import com.badlogic.gdx.math.Interpolation
-import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.scenes.scene2d.actions.Actions
 import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction
 
