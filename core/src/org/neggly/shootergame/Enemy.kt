02a6e344ec96e208e0d2613f9914192d81ce9051
negset <negset1858@gmail.com>
2018-08-01 01:23:49 +0900

    Refactoring

--- core/src/org/neggly/shootergame/Enemy.kt
+++ core/src/org/neggly/shootergame/Enemy.kt
@@ -11,22 +11,19 @@ class Enemy(texture: Texture) : GameObject(texture)
 {
     private val mgr by lazy { parent as ObjectMgr }
 
-    var hp = 0
-    private var stayCounter = 0
-
     private enum class State
     { ENTER, STAY, BACK }
 
     private var state = State.ENTER
 
-    private lateinit var enterSeq: SequenceAction
-    private lateinit var backSeq: SequenceAction
-
-    private var shootCount = 0
-
+    var hp = 0
+    private var shootPattern = 0
     private var shootAngle = 0f
 
-    private var shootPattern = 0
+    private var stayCounter = 0
+
+    private lateinit var enter: SequenceAction
+    private lateinit var back: SequenceAction
 
     /** あたり判定用の枠 */
     val bounds = Circle(x, y, 64f)
@@ -35,30 +32,23 @@ class Enemy(texture: Texture) : GameObject(texture)
     {
         super.activate(x, y)
 
-        hp = 30
-        stayCounter = 0
-
-        val amountY = MathUtils.random(500, 1000)
-        val enter = Actions.moveBy(0f, -amountY.toFloat(), 1.5f, Interpolation.fade)
-        val stay1 = Actions.delay(.5f)
-        val stay2 = Actions.delay(.5f)
-        val back = Actions.moveBy(0f, amountY.toFloat(), 1.5f, Interpolation.fade)
-
-        enterSeq = Actions.sequence()
-        enterSeq.addAction(enter)
-        enterSeq.addAction(stay1)
-
-        backSeq = Actions.sequence()
-        backSeq.addAction(stay2)
-        backSeq.addAction(back)
-
-        addAction(enterSeq)
-
         state = State.ENTER
 
-        shootCount = 0
-
+        hp = 30
         shootPattern = MathUtils.random(2)
+
+        stayCounter = 0
+
+        val amountY = MathUtils.random(500f, 1000f)
+        enter = Actions.sequence().apply {
+            addAction(Actions.moveBy(0f, -amountY, 1.5f, Interpolation.fade))
+            addAction(Actions.delay(.5f))
+        }
+        back = Actions.sequence().apply {
+            addAction(Actions.moveBy(0f, amountY, 1.5f, Interpolation.fade))
+            addAction(Actions.delay(.5f))
+        }
+        addAction(enter)
     }
 
     override fun act(delta: Float)
@@ -107,7 +97,7 @@ class Enemy(texture: Texture) : GameObject(texture)
 
     private fun shoot0()
     {
-        if (shootCount == 0)
+        if (stayCounter == 0)
             shootAngle = mgr.getAngleToPlayer(this)
 
         if (stayCounter % 10 == 0)
@@ -116,19 +106,18 @@ class Enemy(texture: Texture) : GameObject(texture)
             mgr.newShot(x, y, shootAngle)
             mgr.newShot(x, y, shootAngle + 20f)
             mgr.shotSe.play()
-            shootCount++
         }
 
-        if (shootCount >= 5)
+        if (stayCounter > 45)
         {
-            addAction(backSeq)
+            addAction(back)
             state = State.BACK
         }
     }
 
     private fun shoot1()
     {
-        if (shootCount == 0)
+        if (stayCounter == 0)
             shootAngle = 180f
 
         if (stayCounter % 4 == 0)
@@ -138,19 +127,18 @@ class Enemy(texture: Texture) : GameObject(texture)
             mgr.newShot(x, y, shootAngle + 120)
             mgr.shotSe.play()
             shootAngle += 20
-            shootCount++
         }
 
-        if (shootCount >= 6)
+        if (stayCounter > 24)
         {
-            addAction(backSeq)
+            addAction(back)
             state = State.BACK
         }
     }
 
     private fun shoot2()
     {
-        if (shootCount == 0)
+        if (stayCounter == 0)
             shootAngle = mgr.getAngleToPlayer(this) - 30
 
         if (stayCounter % 4 == 0 && stayCounter % 24 != 20)
@@ -158,12 +146,11 @@ class Enemy(texture: Texture) : GameObject(texture)
             mgr.newShot(x, y, shootAngle)
             mgr.shotSe.play()
             shootAngle += if (stayCounter / 24 % 2 == 0) 12 else -12
-            shootCount++
         }
 
-        if (shootCount >= 15)
+        if (stayCounter > 64)
         {
-            addAction(backSeq)
+            addAction(back)
             state = State.BACK
         }
     }
